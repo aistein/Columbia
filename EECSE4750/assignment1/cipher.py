55d81666e018f907728485d65ec42874ece92b03
Alex Stein <astein.ny@gmail.com>
2016-10-05 12:51:31 -0500

    plotting works, cuda works, opencl works, just need to resolve blocksize issue for cuda kernel

--- EECSE4750/assignment1/cipher.py
+++ EECSE4750/assignment1/cipher.py
@@ -28,14 +28,15 @@ import pyopencl as cl
 from pyopencl import array
 
 # Imports for PyCUDA
-# import pycuda.driver as cuda
-# import pycuda.autoinit
-# from pycuda.compiler import SourceModule
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
 
 # General Imports
 import numpy as np
 import sys
 import string
+import random
 import math
 import time
 
@@ -61,13 +62,13 @@ def atbashEncodePY(input_string):
 	# map the input to output using the cipher
 	output = ""
 
+	start = time.time()
 	for idx, val in enumerate(input_string):
 		ascii = ord(val)
 		output += chr(ascii + 25 - 2 * (ascii - 65))
+	runtime = time.time() - start
 
-	# print "PY output: ", output
-
-	return
+	return runtime, output
 
 
 def atbashEncodeOCL(input_string):
@@ -84,7 +85,7 @@ def atbashEncodeOCL(input_string):
 	program = cl.Program(context, """
 		__kernel void apply_cipher(__global const char *inputString, __global int *len, __global char *outputString)
 		{
-			// This input will be a 1-D array, so we only need a single ID to work with it
+			// This input will be a 1-D array, so we only need a single ID to work with it (function of block grid)
 
 				int gid_x = get_global_id(0);
 				int gdim_x = get_global_size(0);
@@ -114,7 +115,7 @@ def atbashEncodeOCL(input_string):
 	size = np.float32(length)
 
 	# max_size = device_id.max_work_group_size
-	max_size = 50
+	max_size = 1024
 
 	len_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=size)
 	inputString_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=input_string)
@@ -125,111 +126,144 @@ def atbashEncodeOCL(input_string):
 	global_dim = (max_size, int(math.ceil(length/float(max_size))))
 	local_dim = (max_size,1)
 
+	### Time the function call
+	start = time.time()
 	program.apply_cipher(queue, global_dim, local_dim, inputString_buf, len_buf, outputString_buf)
+	runtime = time.time() - start
 
 	# 8. Move the kernel's output data back to the host memory
 	cl.enqueue_copy(queue, output_string, outputString_buf)
 
-	return
+	return runtime, output_string
 
 
-# def atbashEncodeCUDA(input_string):
-# 	op = np.empty_like(input_string)
-# 	n = len(input_string)
-#
-# 	mod = SourceModule("""
-# 	    __global__ void apply_cipher(const char* inp, char* op, int len)
-# 	    {
-# 	        int idx = threadIdx.x;
-# 	        float ASCII = 0.0;
-# 	        if ( idx <= len ){
-# 	            ASCII = (float) inp[idx];
-# 	            op[idx] = ASCII + 25.0 - 2 * ( ASCII - 65.0 );
-# 	        }
-# 	    }
-# 	    """)
-#
-# 	apply_cipher = mod.get_function("apply_cipher")
-# 	# max_size = pycuda.autoinit.device.get_attribute(max_threads_per_block)
-#         max_size = 1024
-# 	if n > max_size :
-# 		input_strings = [input_string[i:i+max_size] for i in range(0, len(input_string), max_size)]
-# 		output_string_n = ""
-# 		for div in input_strings :
-# 			op_div = np.empty_like(div)
-# 			n_div = len(div)
-# 			size_div = n_div * sys.getsizeof(' ')
-#
-# 			inp_gpu = cuda.mem_alloc(size_div)
-# 			op_gpu = cuda.mem_alloc(size_div)
-#
-# 			cuda.memcpy_htod(inp_gpu, div)
-# 			cuda.memcpy_htod(op_gpu, op_div)
-# 			apply_cipher(inp_gpu, op_gpu, np.uint32(n_div), block=(n_div,1,1))
-#
-# 			cuda.memcpy_dtoh(op_div, op_gpu)
-# 			output_string_n += str(op_div)
-#
-# 		print "CUDA output: ", output_string_n
-# 	else :
-# 		size = n * sys.getsizeof(' ')
-#
-# 		inp_gpu = cuda.mem_alloc(size)
-# 		op_gpu = cuda.mem_alloc(size)
-#
-# 		cuda.memcpy_htod(inp_gpu, input_string)
-# 		cuda.memcpy_htod(op_gpu, op)
-# 		apply_cipher(inp_gpu, op_gpu, np.uint32(n), block=(n,1,1))
-#
-# 		cuda.memcpy_dtoh(op, op_gpu)
-#
-# 		print "CUDA output: ", op
-#
-# 	return
+def atbashEncodeCUDA(input_string):
+	op = np.empty_like(input_string)
+	n = len(input_string)
+
+	mod = SourceModule("""
+	    __global__ void apply_cipher(const char* inp, char* op, int len)
+	    {
+	        int idx = threadIdx.x;
+	        float ASCII = 0.0;
+	        if ( idx <= len ){
+	            ASCII = (float) inp[idx];
+	            op[idx] = ASCII + 25.0 - 2 * ( ASCII - 65.0 );
+	        }
+	    }
+	    """)
+
+	apply_cipher = mod.get_function("apply_cipher")
+
+	# max_size = pycuda.autoinit.device.get_attribute(max_threads_per_block)
+	max_size = 1024
+
+	size = n * sys.getsizeof('A')
+
+	inp_gpu = cuda.mem_alloc(size)
+	op_gpu = cuda.mem_alloc(size)
+
+	cuda.memcpy_htod(inp_gpu, input_string)
+	cuda.memcpy_htod(op_gpu, op)
+
+	b_size = n if (n <= max_size) else max_size
+
+	start = time.time()
+	apply_cipher(inp_gpu, op_gpu, np.uint32(n), block=(b_size, 1, 1))
+	runtime = time.time() - start
+
+	cuda.memcpy_dtoh(op, op_gpu)
+
+
+	return runtime, op
 
 def nTest(input_string, n, lang="PYTHON"):
-	n_string = ""
-	for x in range(n):
-		n_string += input_string
+	# n_string = ''.join(random.choice(string.ascii_uppercase) for _ in range(n))
+	# print "nString: ", n_string
+
+	n_string = input_string
 
 	if ( lang == "PYTHON" ):
-		atbashEncodePY(n_string)
+		runtime, output = atbashEncodePY(n_string)
 	elif ( lang == "OPENCL" ):
-		atbashEncodeOCL(n_string)
+		runtime, output = atbashEncodeOCL(n_string)
 	elif ( lang == "CUDA" ):
-		atbashEncodeCUDA(n_string)
+		runtime, output = atbashEncodeCUDA(n_string)
 
-	return
+	return runtime, output
 
 #########################################################################################################################
 # Main
 #########################################################################################################################
 
 
-def main(_outdir_="/etl_output/"):
+def main(_repetitions_=1):
 
-	'''Default arguments: _outdir_ - where output files will go'''
+	'''Default arguments: _repetitions_ - how many times to repeat input string'''
 
-	input_string = "K"
-	repetitions = 100077599
+	# input_string = "ALEX"
 
-	M = 3
-	times = []
-	for i in xrange(M):
-		start = time.time()
-		nTest(input_string, repetitions, "PYTHON")
-		times.append(time.time()-start)
-	print 'python time:  ', np.average(times)
+	# Generate a random uppercase ASCII string of length _repetitions_
+	input_string = ''.join(random.choice(string.ascii_uppercase) for _ in range(_repetitions_))
+	# print "INPUT: ", input_string
 
-	times = []
-	for i in xrange(M):
-		start = time.time()
-		nTest(input_string, repetitions, "OPENCL")
-		times.append(time.time()-start)
-	print 'opencl time:  ', np.average(times)
+	pytime, pyout = nTest(input_string, _repetitions_, "PYTHON")
+	ocltime, oclout = nTest(input_string, _repetitions_, "OPENCL")
+	ctime, cout = nTest(input_string, _repetitions_, "CUDA")
 
+	print "PYTHON results: ", pytime, pyout
+	print "OpenCL results: ", ocltime, oclout
+	print "CUDA results: ", ctime, cout
 
+	print "Outputs Equal." if (pyout == str(oclout)) and (pyout == str(cout)) else "Outputs Unequal."
+
+	TEST_ALL = False
+	if TEST_ALL :
+		M = 3
+		python_times = []
+		ocl_times = []
+		cuda_times = []
+
+		for k in xrange(1,_repetitions_) :
+
+			input_string = ''.join(random.choice(string.ascii_uppercase) for _ in range(k))
+
+			python_times_tmp = []
+			ocl_times_tmp = []
+			cuda_times_tmp = []
+
+			for i in xrange(M):
+
+				pytime, pyout = nTest(input_string, k, "PYTHON")
+				ocltime, oclout = nTest(input_string, k, "OPENCL")
+				ctime, cout = nTest(input_string, k, "CUDA")
+
+				python_times_tmp.append(pytime)
+				ocl_times_tmp.append(ocltime)
+				cuda_times_tmp.append(ctime)
+
+			python_times.append(np.average(python_times_tmp))
+			ocl_times.append(np.average(ocl_times_tmp))
+			cuda_times.append(np.average(cuda_times_tmp))
+
+		MAKE_PLOT = True
+		if MAKE_PLOT:
+			import matplotlib as mpl
+			mpl.use('agg')
+			import matplotlib.pyplot as plt
+			x = list(xrange(1,_repetitions_))
+
+			plt.gcf()
+			plt.plot(x, python_times, color='r', label='python')
+			plt.plot(x, ocl_times, color='b', label='openCL')
+			plt.plot(x, cuda_times, color='g', label='CUDA')
+			plt.xlabel('length')
+			plt.ylabel('time')
+			plt.legend(loc='upper left')
+			plt.gca().set_xlim((min(x), max(x)))
+			plt.gca().set_ylim((min(python_times)/2, 2*max(ocl_times)))
+			plt.savefig('python_times.png')
 
 
 if __name__ == '__main__':
-	main()
+	main(int(sys.argv[1]))
