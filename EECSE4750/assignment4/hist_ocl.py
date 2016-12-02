1d858cb16ce9d3d25b681b5823aac1a140c32208
Alexander Stein <astein.ny@gmail.com>
2016-12-02 08:38:00 -0600

    this isn't working, trying private histo from slides

--- EECSE4750/assignment4/hist_ocl.py
+++ EECSE4750/assignment4/hist_ocl.py
@@ -34,7 +34,7 @@ def hist(x):
 # C = P*3
 # N = R*C
 
-P = 512
+P = 32
 R = P*2
 C = P*3
 N = R*C
@@ -48,6 +48,7 @@ N = R*C
 img_size = N # 1MB for starters
 print "-I- IMAGE SIZE : ", img_size
 img = np.memmap('/opt/data/random.dat', dtype=np.uint8, shape=(R,C), mode='r')
+# print img.flat[6143]
 
 ############################
 ## NAIVE Histogram Kernel ##
@@ -78,22 +79,35 @@ naive_hist.set_scalar_arg_dtypes([None, None, np.uint32])
 
 opt1_hist = cl.Program(ctx, """
 __kernel void opt1_hist(__global unsigned char *img, __global unsigned int *bins,
-                   const unsigned int P) {
+                   const unsigned int P, const unsigned int N) {
+
+    // pragma to enable larger atomic add
+    #pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
+
     unsigned int i = get_global_id(0);
     unsigned int k;
+    unsigned char z;
+    //volatile __local unsigned int bins_loc[256];
     volatile __local unsigned char bins_loc[256];
 
-    for (k=0; k<256; k++)
-        bins_loc[k] = 0;
+    // DEVICE_OPT1 : ???
+    //for (k=0; k<256; k++)
+    if (i < 256)
+        bins_loc[i] = 0;
+    //barrier(CLK_LOCAL_MEM_FENCE);
     for (k=0; k<P; k++)
-        ++bins_loc[img[i*P+k]];
+        //++bins_loc[img[i*P+k]];
+        //z = img[i*P+k];
+        //barrier(CLK_LOCAL_MEM_FENCE);
+        bins_loc[img[i*P+k]] = bins_loc[img[i*P+k]] + 1;
+        //atom_add(&bins_loc[z], 1);
     barrier(CLK_LOCAL_MEM_FENCE);
     for (k=0; k<256; k++)
         atomic_add(&bins[k], bins_loc[k]);
 }
 """).build().opt1_hist
 
-opt1_hist.set_scalar_arg_dtypes([None, None, np.uint32])
+opt1_hist.set_scalar_arg_dtypes([None, None, np.uint32, np.uint32])
 
 ###########################
 ## END Histogram Kernels ##
@@ -116,17 +130,20 @@ print time.time()-start
 # print np.allclose(h_py, h_op)
 
 # Time Opt1 OpenCL function: ALLOC_HOST_PTR
-# OPT1: using memory pool to keep memory allocated
-# OPT2: using strides to coalesce dram accesses
-# OPT3: change order from 'C' column major to 'R' row major
+# HOST_OPT1: using memory pool to keep memory allocated (pinned)
+# HOST_OPT2: using strides to coalesce dram accesses (auto-calculated by Array.__init__())
 mem_pool = cl.tools.MemoryPool(cl.tools.ImmediateAllocator(queue))
-img_gpu_opt = cl.array.Array(queue, img, allocator=mem_pool, strides=(32,), order='R')
-img_gpu_opt.to_device()
+img_gpu_opt = cl.array.Array(queue, img.shape, np.uint8, allocator=mem_pool)
+print img_gpu_opt.astype(np.uint32)
+img_gpu_opt.set(img)
+# img_gpu_opt = cl.array.to_device(queue, img)
 bin_gpu_opt = cl.array.zeros(queue, 256, np.uint32)
 start = time.time()
-opt1_hist(queue, (N/32,), (1,), img_gpu_opt.data, bin_gpu_opt.data, 32)
+opt1_hist(queue, (N/32,), (1,), img_gpu_opt.data, bin_gpu_opt.data, 32, N)
 print time.time()-start
 h_op_opt =  bin_gpu_opt.get()
+print "opencl: ",h_op_opt
+print "python: ",h_py
 
 # check opt1 correctness:
 print np.allclose(h_py, h_op_opt)
