68fb520e52b92eb61b65dd3d6a491cf097260ccc
Alex Stein <astein.ny@gmail.com>
2018-04-11 13:06:42 -0400

    q2c complete

--- STATGU4001/hw6/earnings_regression.py
+++ STATGU4001/hw6/earnings_regression.py
@@ -16,66 +16,91 @@ import matplotlib.pyplot as plt
 #
 # f(sex) = 1 if sex = woman and 0 otherwise
 
+####################################################################################
+def linear_model( earnings_data , namestring ):
+    """ takes in earnings_data as a numpy array, prints linear model information """
+    # x - height, y - log-earnings
+    n = earnings_data.shape[0]
+    x_hat = np.mean(earnings_data[:,2])
+    s_x = np.std(earnings_data[:,2])
+    y_hat = np.mean(earnings_data[:,1])
+    s_y = np.std(earnings_data[:,1])
+
+    # calculate the linear model parameters SxY, Sxx, SYY, and use them to
+    # find the estimators A and B, the correlation, and the residuals
+    S_xY = S_xx = S_YY = 0.0
+    for i in range(0,n-1):
+        S_xY += (earnings_data[i,2] - x_hat)*(earnings_data[i,1] - y_hat)
+        S_xx += (earnings_data[i,2] - x_hat)**2
+        S_YY += (earnings_data[i,1] - y_hat)**2
+    B = S_xY / S_xx
+    A = y_hat - B * x_hat
+    SS_R = (S_xx * S_YY - S_xY**2) / S_xx
+    R_sq = 1 - SS_R / S_YY
+    r = S_xY / (np.sqrt(S_xx * S_YY))
+
+    print("x_hat =", x_hat)
+    print("s_x =", s_x)
+    print("y_hat =", y_hat)
+    print("e^(y_hat) =", np.exp(y_hat))
+    print("s_y =", s_y)
+    print("e^(s_y) =", np.exp(s_y))
+    print("R_sq =", R_sq)
+    print("r =",r)
+    print("S_YY =", S_YY)
+    print("SS_R =", SS_R)
+
+    print("Linear model Y = A + Bx, with A=", A, " and B=", B)
+    print("Increase in $-earnings/in is ", np.exp(B)*100 - 100, "%")
+
+    # calculate and plot the normalized residuals
+    residuals = np.zeros((n),dtype=np.float32)
+    for i in range(0,n-1):
+        Yi = earnings_data[i,1]
+        Xi = earnings_data[i,2]
+        residuals[i] = (Yi - A - B * Xi) / (SS_R / (n - 2))
+
+    plt.subplot(2,1,1)
+    plt.plot(earnings_data[:,2], np.zeros((n)), color='red')
+    plt.scatter(earnings_data[:,2], residuals, color='green')
+    plt.ylabel("normalized residual")
+    plt.title("Linear Regression: Height vs. Earnings (" + namestring + ")")
+
+    plt.subplot(2,1,2)
+    plt.plot(earnings_data[:,2], A + B * (earnings_data[:,2]), color='red')
+    plt.scatter(earnings_data[:,2], earnings_data[:,1], color='blue')
+    plt.ylabel("earnings (log-dollars)")
+    plt.xlabel("height (in)")
+
+    plt.savefig('residuals_2c_' + namestring + '.png')
+    plt.close()
+
+    return
+####################################################################################
+
+
 earnings_data = np.genfromtxt('heights.csv', delimiter=',', dtype=np.float32)
 
 # part a: transform earnings into log-earnings
 for person in earnings_data:
     person[1] = np.log(person[1])
     person[2] = 12 * person[2] + person[3] # convert to inches
-    # print("height: ", person[2], "\" log-earn: ", person[1])
 
-# x - height, y - log-earnings
-n = earnings_data.shape[0]
-x_hat = np.mean(earnings_data[:,2])
-s_x = np.std(earnings_data[:,2])
-y_hat = np.mean(earnings_data[:,1])
-s_y = np.std(earnings_data[:,1])
+# part c: cut into two sets male and female, create two linear models
+earnings_male = []
+earnings_female = []
+for person in earnings_data:
+    if (person[4] == 1.0):
+        earnings_male.append(person)
+    if (person[4] == 2.0):
+        earnings_female.append(person)
 
-# calculate the linear model parameters SxY, Sxx, SYY, and use them to
-# find the estimators A and B, the correlation, and the residuals
-S_xY = S_xx = S_YY = 0.0
-for i in range(0,n-1):
-    S_xY += (earnings_data[i,2] - x_hat)*(earnings_data[i,1] - y_hat)
-    S_xx += (earnings_data[i,2] - x_hat)**2
-    S_YY += (earnings_data[i,1] - y_hat)**2
-B = S_xY / S_xx
-A = y_hat - B * x_hat
-SS_R = (S_xx * S_YY - S_xY**2) / S_xx
-R_sq = 1 - SS_R / S_YY
-r = S_xY / (np.sqrt(S_xx * S_YY))
+earnings_male = np.asarray(earnings_male)
+earnings_female = np.asarray(earnings_female)
 
-print("x_hat =", x_hat)
-print("s_x =", s_x)
-print("y_hat =", y_hat)
-print("e^(y_hat) =", np.exp(y_hat))
-print("s_y =", s_y)
-print("e^(s_y) =", np.exp(s_y))
-print("R_sq =", R_sq)
-print("r =",r)
-print("S_YY =", S_YY)
-print("SS_R =", SS_R)
-
-print("Linear model Y = A + Bx, with A=", A, " and B=", B)
-print("Increase in $-earnings/in is ", np.exp(B)*100 - 100, "%")
-
-# calculate and plot the normalized residuals
-residuals = np.zeros((n),dtype=np.float32)
-for i in range(0,n-1):
-    Yi = earnings_data[i,1]
-    Xi = earnings_data[i,2]
-    residuals[i] = (Yi - A - B * Xi) / (SS_R / (n - 2))
-
-plt.subplot(2,1,1)
-plt.plot(earnings_data[:,2], np.zeros((n)), color='red')
-plt.scatter(earnings_data[:,2], residuals, color='green')
-plt.ylabel("normalized residual")
-plt.title("Linear Regression: Height vs. Earnings")
-
-plt.subplot(2,1,2)
-plt.plot(earnings_data[:,2], A + B * (earnings_data[:,2]), color='red')
-plt.scatter(earnings_data[:,2], earnings_data[:,1], color='blue')
-plt.ylabel("earnings (log-dollars)")
-plt.xlabel("height (in)")
-
-plt.savefig('residuals_2c.png')
-plt.show()
+# males
+print("\nMALE LINEAR MODEL\n")
+linear_model(earnings_male, "Male")
+# females
+print("\nFEMALE LINEAR MODEL\n")
+linear_model(earnings_female, "Female")
