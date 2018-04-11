b9cb6bca4d91e1f7de6bb9bfc94665d878b5db66
Alex Stein <astein.ny@gmail.com>
2018-04-11 16:10:53 -0400

    99% done

--- STATGU4001/hw6/earnings_regression.py
+++ STATGU4001/hw6/earnings_regression.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 # f(sex) = 1 if sex = woman and 0 otherwise
 
 ####################################################################################
-def linear_model( earnings_data , namestring ):
+def linear_model( earnings_data , namestring , print_stats=True):
     """ takes in earnings_data as a numpy array, prints linear model information """
     # x - height, y - log-earnings
     n = earnings_data.shape[0]
@@ -39,16 +39,17 @@ def linear_model( earnings_data , namestring ):
     R_sq = 1 - SS_R / S_YY
     r = S_xY / (np.sqrt(S_xx * S_YY))
 
-    print("x_hat =", x_hat)
-    print("s_x =", s_x)
-    print("y_hat =", y_hat)
-    print("e^(y_hat) =", np.exp(y_hat))
-    print("s_y =", s_y)
-    print("e^(s_y) =", np.exp(s_y))
-    print("R_sq =", R_sq)
-    print("r =",r)
-    print("S_YY =", S_YY)
-    print("SS_R =", SS_R)
+    if(print_stats):
+        print("x_hat =", x_hat)
+        print("s_x =", s_x)
+        print("y_hat =", y_hat)
+        print("e^(y_hat) =", np.exp(y_hat))
+        print("s_y =", s_y)
+        print("e^(s_y) =", np.exp(s_y))
+        print("R_sq =", R_sq)
+        print("r =",r)
+        print("S_YY =", S_YY)
+        print("SS_R =", SS_R)
 
     print("Linear model Y = A + Bx, with A=", A, " and B=", B)
     print("Increase in $-earnings/in is ", np.exp(B)*100 - 100, "%")
@@ -75,9 +76,13 @@ def linear_model( earnings_data , namestring ):
     plt.savefig('residuals_2c_' + namestring + '.png')
     plt.close()
 
-    return
+    return (A, B)
 ####################################################################################
-
+####################################################################################
+def prediction(Xi,params):
+    """ return Yi prediction based on the provided parameters """
+    return params[0] + params[1] * Xi
+####################################################################################
 
 earnings_data = np.genfromtxt('heights.csv', delimiter=',', dtype=np.float32)
 
@@ -98,9 +103,47 @@ for person in earnings_data:
 earnings_male = np.asarray(earnings_male)
 earnings_female = np.asarray(earnings_female)
 
+# agnostic
+print("\nAGNOSTIC MODEL\n")
+a_params = linear_model(earnings_data, "Agnostic")
 # males
 print("\nMALE LINEAR MODEL\n")
-linear_model(earnings_male, "Male")
+m_params = linear_model(earnings_male, "Male")
 # females
 print("\nFEMALE LINEAR MODEL\n")
-linear_model(earnings_female, "Female")
+f_params = linear_model(earnings_female, "Female")
+
+# joint model
+n = earnings_data.shape[0]
+earnings_predictions = []
+for man in earnings_male:
+    man[1] = prediction(man[2],m_params)
+    earnings_predictions.append(man)
+for woman in earnings_female:
+    woman[1] = prediction(woman[2],f_params)
+    earnings_predictions.append(woman)
+earnings_predictions = np.asarray(earnings_predictions)
+print("\nJOINT MODEL\n")
+j_params = linear_model(earnings_predictions, "Joint", print_stats=False)
+
+#finally plot the joint model against the original data
+residuals = np.zeros((n),dtype=np.float32)
+for i in range(0,n-1):
+    Yi = earnings_data[i,1]
+    Xi = earnings_data[i,2]
+    residuals[i] = (Yi - j_params[0] - j_params[1] * Xi)
+
+plt.subplot(2,1,1)
+plt.plot(earnings_data[:,2], np.zeros((n)), color='red')
+plt.scatter(earnings_data[:,2], residuals, color='green')
+plt.ylabel("normalized residual")
+plt.title("Linear Regression: Height vs. Earnings (Joint)")
+
+plt.subplot(2,1,2)
+plt.plot(earnings_data[:,2], j_params[0] + j_params[1] * (earnings_data[:,2]), color='red')
+plt.scatter(earnings_data[:,2], earnings_data[:,1], color='blue')
+plt.ylabel("earnings (log-dollars)")
+plt.xlabel("height (in)")
+
+plt.savefig('residuals_2e_Joint.png')
+plt.show()
